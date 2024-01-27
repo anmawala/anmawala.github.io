171ed8db78fc85d845b98a22e6e0f07a86401bf7
Antonio L. <antonio.laviola@gmail.com>
2024-01-27 22:56:32 +0100

    New Version

---
+++ js/js_functions.js
@@ -0,0 +1,304 @@
+var isUpdateAvailable = false;
+
+getIsUpdateAvailable = function () {
+    return isUpdateAvailable;
+}
+
+startAppUpdateFlow = async function () {
+    reg = await navigator.serviceWorker.getRegistration();
+    var userResponse = confirm("Premi Ok per rendere effettivo l'aggiornamento dell'applicazione. L'applicazione verrà riavviata. Se l'applicazione viene chiusa definitivamente, alla riapertura essa sarà comunque aggiornata.");
+
+    if (userResponse) {
+        if (reg.waiting) {
+            // let waiting Service Worker know it should became active
+            reg.waiting.postMessage('SKIP_WAITING')
+        }
+    }
+    isUpdateAvailable = false;
+}
+
+checkGeolocationPermission = function () {
+    return navigator.permissions.query({ name: "geolocation" }).then((result) => {
+        return result.state;
+    });
+}
+
+testGeolocation = function () {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            resolve("granted");
+        }, function (error) {
+            reject(error.code);
+        });
+
+        // Reject the promise after 10 seconds
+        setTimeout(() => {
+            reject('Overall timeout exceeded');
+        }, 10000);
+    });
+}
+
+startWatchingPosition = function (minAccuracy) {
+    return new Promise((resolve, reject) => {
+        const id = navigator.geolocation.watchPosition(function (position) {
+
+            newPosition = {
+                coords: {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    accuracy: position.coords.accuracy
+                },
+                timestamp: position.timestamp
+            }
+
+            if (position.coords.accuracy <= minAccuracy) {
+                resolve(newPosition);
+                navigator.geolocation.clearWatch(id);
+            }
+        }, function (error) {
+            reject(error.code);
+        }, { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 });
+
+        // Reject the promise after 10 seconds
+        setTimeout(() => {
+            navigator.geolocation.clearWatch(id);
+            reject('Overall timeout exceeded');
+        }, 10000);
+    });
+}
+
+saveToLocalStorage = function (key, value) {
+    localStorage.setItem(key, value);
+}
+readFromLocalStorage = function (key) {
+    return localStorage.getItem(key);
+}
+
+showModal = function (modalId) {
+    var myModal = new bootstrap.Modal(document.getElementById(modalId), {});
+    myModal.show();
+}
+
+checkConnection = async function () {
+    try {
+        const response = await fetch("https://www.google.com", { mode: 'no-cors' });
+        // Se la richiesta non lancia un'eccezione, restituisci true
+        return true;
+    } catch (error) {
+        // Se c'è un errore nella richiesta, restituisci false
+        return false;
+    }
+}
+
+hideElement = function (elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.style.display = "none";
+    }
+}
+
+showElement = function (elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.style.display = "block";
+    }
+}
+
+showToast = function (toastId) {
+    var toastEl = document.getElementById(toastId);
+    if (toastEl) {
+        var toast = new bootstrap.Toast(toastEl, { delay: 2000 });
+        toast.show();
+    }
+}
+
+
+function onupgradeneeded(event) {
+    const db = event.target.result;
+            const objectStore = db.createObjectStore("Stampings", { keyPath: "id", autoIncrement: true });
+            objectStore.createIndex("userId", "userId", { unique: false });
+            objectStore.createIndex("clockingIn", "clockingIn", { unique: false });
+            objectStore.createIndex("clockingOut", "clockingOut", { unique: false });
+            objectStore.createIndex("userEnablementId", "userEnablementId", { unique: false });
+            objectStore.createIndex("structureId", "structureId", { unique: false });
+            objectStore.createIndex("StructureName", "StructureName", { unique: false });
+            objectStore.createIndex("enablementId", "enablementId", { unique: false });
+            objectStore.createIndex("enablementName", "enablementName", { unique: false });
+            objectStore.createIndex("enablementStartTime", "oraInizioPrestazione", { unique: false });
+            objectStore.createIndex("enablementEndTime", "enablementEndTime", { unique: false });
+        };
+
+
+clockingIn = function ($p) {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open("Stampings");
+
+        request.onupgradeneeded = onupgradeneeded;
+
+        request.onsuccess = function (event) {
+            const db = event.target.result;
+            const transaction = db.transaction(["Stampings"], "readwrite");
+            const objectStore = transaction.objectStore("Stampings");
+
+            const stamp = {
+                userId : $p.userId,
+                clockingIn: $p.clockingIn,
+                clockingOut: null,
+                userEnablementId: $p.userEnablementId,
+                structureId: $p.structureId,
+                structureName: $p.structureName,
+                enablementId: $p.enablementId,
+                enablementName: $p.enablementName,
+                enablementStartTime: $p.enablementStartTime,
+                enablementEndTime: $p.enablementEndTime
+            };
+
+            const requestAdd = objectStore.add(stamp);
+
+            requestAdd.onsuccess = function (event) {
+                resolve(true);
+            };
+
+            requestAdd.onerror = function (event) {
+                reject(false);
+            };
+        };
+
+        request.onerror = function (event) {
+            reject(false);
+        };
+    });
+}
+
+getStamps = function (userId, inOnly = false) {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open("Stampings");
+
+        request.onupgradeneeded = onupgradeneeded;
+
+        request.onsuccess = function (event) {
+            const db = event.target.result;
+            const transaction = db.transaction(["Stampings"], "readonly");
+            const objectStore = transaction.objectStore("Stampings");
+
+            const index = objectStore.index("userId");
+            const requestGet = index.openCursor(IDBKeyRange.only(userId));
+            const results = [];
+
+            requestGet.onsuccess = function (event) {
+                const cursor = event.target.result;
+                if (cursor) {
+                    const record = cursor.value;
+                    if(inOnly) {
+                        if (record && record.userId === userId && record.clockingOut === null) {
+                            results.push(record);
+                        }
+                    }else{
+                        if (record && record.userId === userId) {
+                            results.push(record);
+                        }
+                    }
+                    
+                    cursor.continue();
+                } else {
+                    // Sort the results by ClockingIn from the most recent to the oldest
+                    results.sort((a, b) => new Date(b.clockingIn) - new Date(a.clockingIn));
+                    resolve(results);
+                }
+            };
+
+            requestGet.onerror = function (event) {
+                reject(false); // Error retrieving records
+            }
+        };
+
+        request.onerror = function (event) {
+            reject(false); // Error opening the database
+        };
+    });
+}
+
+deleteStamp = function (id) {
+    const request = indexedDB.open("Stampings");
+
+    request.onupgradeneeded = onupgradeneeded;
+
+    request.onsuccess = function (event) {
+        const db = event.target.result;
+        const transaction = db.transaction(["Stampings"], "readwrite");
+        const objectStore = transaction.objectStore("Stampings");
+
+        const requestDelete = objectStore.delete(id);
+        requestDelete.onsuccess = function (event) {
+            console.log("Record deleted successfully");
+        };
+        requestDelete.onerror = function (event) {
+            console.error("Error deleting record");
+        };
+    };
+
+    request.onerror = function (event) {
+        console.error("Error opening database");
+    };
+}
+
+
+clockingOut = function (id, clockingOut) {
+    const request = indexedDB.open("Stampings");
+
+    request.onupgradeneeded = onupgradeneeded;
+
+    request.onsuccess = function (event) {
+        const db = event.target.result;
+        const transaction = db.transaction(["Stampings"], "readwrite");
+        const objectStore = transaction.objectStore("Stampings");
+
+        const requestGet = objectStore.get(id);
+        requestGet.onsuccess = function (event) {
+            const record = event.target.result;
+            if (record) {
+                record.clockingOut = clockingOut;
+                const requestUpdate = objectStore.put(record);
+                requestUpdate.onsuccess = function (event) {
+                    console.log("Record updated successfully");
+                };
+                requestUpdate.onerror = function (event) {
+                    console.error("Error updating record");
+                };
+            } else {
+                console.error("Record not found");
+            }
+        };
+        requestGet.onerror = function (event) {
+            console.error("Error retrieving record");
+        };
+    };
+
+    request.onerror = function (event) {
+        console.error("Error opening database");
+    };
+}
+
+deleteStamp = function (id) {
+    const request = indexedDB.open("Stampings");
+
+    request.onupgradeneeded = onupgradeneeded;
+
+    request.onsuccess = function (event) {
+        const db = event.target.result;
+        const transaction = db.transaction(["Stampings"], "readwrite");
+        const objectStore = transaction.objectStore("Stampings");
+
+        const requestDelete = objectStore.delete(id);
+        requestDelete.onsuccess = function (event) {
+            console.log("Record deleted successfully");
+        };
+        requestDelete.onerror = function (event) {
+            console.error("Error deleting record");
+        };
+    };
+
+    request.onerror = function (event) {
+        console.error("Error opening database");
+    };
+}
---
+++ service-worker-assets.js
@@ -0,0 +1,445 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-UShNUpFHIbzIjIiLBrkynmL6qe\/dAzhN4swMD9Mfwvw=",
+      "url": "bootstrap\/css\/bootstrap-grid.css"
+    },
+    {
+      "hash": "sha256-4LSLadNEjteFdES8BvRm9b4rmdGmXnw8p8DP0kj7O9I=",
+      "url": "bootstrap\/css\/bootstrap-grid.css.map"
+    },
+    {
+      "hash": "sha256-\/fgwtSnmPD3eVfdzl3urLT5535LT6iPau24LJM2k9DE=",
+      "url": "bootstrap\/css\/bootstrap-grid.min.css"
+    },
+    {
+      "hash": "sha256-HAe12x12UDlpMgLQ455YZxmh+pW+LwDWwlCr+qDqcnk=",
+      "url": "bootstrap\/css\/bootstrap-grid.min.css.map"
+    },
+    {
+      "hash": "sha256-Zb2P33aQCHAhY+vXjMZfP9w1Z8oOa1cg+CmeVF7S4Cs=",
+      "url": "bootstrap\/css\/bootstrap-grid.rtl.css"
+    },
+    {
+      "hash": "sha256-\/Dbsv59Zkf74NwKZBWNkfk1Sj51MfSXpyV8gVG4l+LU=",
+      "url": "bootstrap\/css\/bootstrap-grid.rtl.css.map"
+    },
+    {
+      "hash": "sha256-5HTrWqWqjI+F4M1C6AGaYZLiJxdzKG292Hk9etMTXdg=",
+      "url": "bootstrap\/css\/bootstrap-grid.rtl.min.css"
+    },
+    {
+      "hash": "sha256-+Y5Lp6LzAd2J39XaIYnyVR3BrTCVfVVSKQBRlU5VQVk=",
+      "url": "bootstrap\/css\/bootstrap-grid.rtl.min.css.map"
+    },
+    {
+      "hash": "sha256-HKM+szsju2xPmskqqcIbaaJUPy6Nyj\/+aC02aJ1PUDc=",
+      "url": "bootstrap\/css\/bootstrap-reboot.css"
+    },
+    {
+      "hash": "sha256-paYTr0+aZ6OdtEog2Gm3GmcwlD1WUAh8Zo519epCOYk=",
+      "url": "bootstrap\/css\/bootstrap-reboot.css.map"
+    },
+    {
+      "hash": "sha256-mdTOzHm8F\/Z2yGUUywJTUpHjauM8IdFchQpEh7vg6Oo=",
+      "url": "bootstrap\/css\/bootstrap-reboot.min.css"
+    },
+    {
+      "hash": "sha256-T2TUf67\/3Qp7PqFMIwNy1hEXmtUaLvg6Eo6dU37wd8U=",
+      "url": "bootstrap\/css\/bootstrap-reboot.min.css.map"
+    },
+    {
+      "hash": "sha256-seP5\/HpJcS4OWBCxkhsaR3jLJfaVoLuAOcr7R61UMAw=",
+      "url": "bootstrap\/css\/bootstrap-reboot.rtl.css"
+    },
+    {
+      "hash": "sha256-EaJHBjzWe9qmy\/9hOjOsABID38Np2tYK6CKGXM4itRM=",
+      "url": "bootstrap\/css\/bootstrap-reboot.rtl.css.map"
+    },
+    {
+      "hash": "sha256-LWWJ+xba3J6LGlyqqI3xfEldhyLUZWYTLecegI1wzNI=",
+      "url": "bootstrap\/css\/bootstrap-reboot.rtl.min.css"
+    },
+    {
+      "hash": "sha256-0rmIg0ZcTQWL8M52oEXUcVWki18Qu6YoklBRHybbScU=",
+      "url": "bootstrap\/css\/bootstrap-reboot.rtl.min.css.map"
+    },
+    {
+      "hash": "sha256-pTwMJ9t1MB2tw+yDaAfJZmzlK8oTUPOQyHF1CMFVxok=",
+      "url": "bootstrap\/css\/bootstrap-utilities.css"
+    },
+    {
+      "hash": "sha256-zQbBFm3XuoSMmfmOjlm3mefzcYZ1KXDFKbJkY78NiIU=",
+      "url": "bootstrap\/css\/bootstrap-utilities.css.map"
+    },
+    {
+      "hash": "sha256-ksEHyZIfCwhILlq\/+Nqg7W7TA4atPjiprZ+J\/s+fnek=",
+      "url": "bootstrap\/css\/bootstrap-utilities.min.css"
+    },
+    {
+      "hash": "sha256-0cgQAf0bCThD0wOrbcEdPiHtkjEW2UcHRMyYpnovsoc=",
+      "url": "bootstrap\/css\/bootstrap-utilities.min.css.map"
+    },
+    {
+      "hash": "sha256-QklTeW+wXqOca6mBmXsu9TrB\/jMnZzs3bbJYc5zPYBs=",
+      "url": "bootstrap\/css\/bootstrap-utilities.rtl.css"
+    },
+    {
+      "hash": "sha256-r3tFVjZsNgmqDbYDMJq21NePa3YbBx+tPu0cmu6Sik8=",
+      "url": "bootstrap\/css\/bootstrap-utilities.rtl.css.map"
+    },
+    {
+      "hash": "sha256-A2UtwjOawsQ3qcP4EdmW15pEQnf6Q1xnJl9aQ2GVynY=",
+      "url": "bootstrap\/css\/bootstrap-utilities.rtl.min.css"
+    },
+    {
+      "hash": "sha256-2J14miDZ1XoiCaHssRj+BWq19VEOuygwXTMZnc+fFgY=",
+      "url": "bootstrap\/css\/bootstrap-utilities.rtl.min.css.map"
+    },
+    {
+      "hash": "sha256-Fu3WLwzq+Wno4sD0N7TecpEnj4BjkYmdOx9rFT9OYh8=",
+      "url": "bootstrap\/css\/bootstrap.css"
+    },
+    {
+      "hash": "sha256-vf7hQ6dTtGIyw2A1XM2\/5toLVxzQ4U03H6QDM\/IuYww=",
+      "url": "bootstrap\/css\/bootstrap.css.map"
+    },
+    {
+      "hash": "sha256-pMEjVRQy8QqWW4qfcG06j57TbhVkYg9SDeZM31v+bck=",
+      "url": "bootstrap\/css\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-jJm07VkL1A9mdCv66\/smd1yoA9iCc7h7nfn53AX38cc=",
+      "url": "bootstrap\/css\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-xaV8xHsMsmhcMiOrX+JxB13kEKOiwDUaUiklpc0cioY=",
+      "url": "bootstrap\/css\/bootstrap.rtl.css"
+    },
+    {
+      "hash": "sha256-aSa3UqjvTKcwcD3VyLuRQNWSAvZhS9+9o5OeksQNDfs=",
+      "url": "bootstrap\/css\/bootstrap.rtl.css.map"
+    },
+    {
+      "hash": "sha256-VhmVB1lmzejsorhk4ur4+T9LaDqcPcb4h3HBJL3tZAs=",
+      "url": "bootstrap\/css\/bootstrap.rtl.min.css"
+    },
+    {
+      "hash": "sha256-ok+MdFuCruf65Q\/YRx6n4nQX5tICGcAVnSpZLD7ALq4=",
+      "url": "bootstrap\/css\/bootstrap.rtl.min.css.map"
+    },
+    {
+      "hash": "sha256-xSANqfmAd+UT7PCLsbXQwMiqct3N6VXo3BdoDi+JixE=",
+      "url": "bootstrap\/js\/bootstrap.bundle.js"
+    },
+    {
+      "hash": "sha256-IxMkta2LpHFGu0RHqo9zeT3p7ris08iYljWium9F0F0=",
+      "url": "bootstrap\/js\/bootstrap.bundle.js.map"
+    },
+    {
+      "hash": "sha256-JDJ424DHaphTAZCH6UKdhuZNr9MC6j5HTngeLbi7vPs=",
+      "url": "bootstrap\/js\/bootstrap.bundle.min.js"
+    },
+    {
+      "hash": "sha256-GaWwuI0k1rFUxEYolsljHAsxfXHyLuu\/C55AXbQ1H9E=",
+      "url": "bootstrap\/js\/bootstrap.bundle.min.js.map"
+    },
+    {
+      "hash": "sha256-4qV3pIl55N4e6Vsgsn93elg9acuT8uGddZHjvC6z3yE=",
+      "url": "bootstrap\/js\/bootstrap.esm.js"
+    },
+    {
+      "hash": "sha256-wzReyM\/u+gHxZNghff+PrrZ+jPjEPMqtTpK\/AQCw030=",
+      "url": "bootstrap\/js\/bootstrap.esm.js.map"
+    },
+    {
+      "hash": "sha256-cGCes6JjVgOJMw5Alx35u7o7dq9eRaxyIPXarTiatPs=",
+      "url": "bootstrap\/js\/bootstrap.esm.min.js"
+    },
+    {
+      "hash": "sha256-Y\/uEY+YnTPffJunzzC4+e\/lgpz2mm56QSIlj8oQFEQo=",
+      "url": "bootstrap\/js\/bootstrap.esm.min.js.map"
+    },
+    {
+      "hash": "sha256-Er2S1T2\/kS0vznbM\/waO8oJ4LNJW5GrS48uGVLLykpA=",
+      "url": "bootstrap\/js\/bootstrap.js"
+    },
+    {
+      "hash": "sha256-PwsqVOWgWAy0Cozm6sVSNnU8+mnrW\/6ooHcRhMv0rEs=",
+      "url": "bootstrap\/js\/bootstrap.js.map"
+    },
+    {
+      "hash": "sha256-+x6VdLCX5+jIZXfqlZ42Ig5lVTyWNp\/BXpi6ZBSYLoQ=",
+      "url": "bootstrap\/js\/bootstrap.min.js"
+    },
+    {
+      "hash": "sha256-GsvnxsqiJ97Twe4gJJSij+XcdDFca4HVzFVC8DIV10c=",
+      "url": "bootstrap\/js\/bootstrap.min.js.map"
+    },
+    {
+      "hash": "sha256-ApvV\/81qZcuxqxfR7MK1gjS8uW+hGhjP+\/HfHaEGzGA=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-HAMkylmROlP1CwK4p4Ybwuz3ojvL6sll+ppH\/bPhcwY=",
+      "url": "images\/icons\/favicon.png"
+    },
+    {
+      "hash": "sha256-W5JYMi21ZyNvTH7WTMz072b2hDOa2DATzX1g3v8+GVg=",
+      "url": "images\/icons\/icon-192.png"
+    },
+    {
+      "hash": "sha256-oDzyRIZ5t97dEFHUCSE1bVePZjpCh960muIiJ86LdmM=",
+      "url": "images\/icons\/icon-512.png"
+    },
+    {
+      "hash": "sha256-dj5DoRHdNTz9R0X0aZ13A5KVQdAkGzDq7n0r9zaJlXM=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-c5aMMWlHqwT0pb3pT27M5ERWdUjsU1A\/bcwb+01t0SI=",
+      "url": "js\/js_functions.js"
+    },
+    {
+      "hash": "sha256-esafAQ6sHDODTl4KKMkD8QNfXxfBi8dkkBhSh33BWR8=",
+      "url": "manifest.webmanifest"
+    },
+    {
+      "hash": "sha256-rcIWTVNnYus6VWf2Z2U1g\/JzlC07Ym02cFjHtI59Xsc=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-mef8UU0yBP8VMzRFOk45tny0G3qw7NZAbXKY7Jrka7Y=",
+      "url": "_framework\/dotnet.js"
+    },
+    {
+      "hash": "sha256-SGOlISx+3G4kkvpC9Uq3qoPGsnjxGmW61QBev0lyI5U=",
+      "url": "_framework\/dotnet.native.8.0.1.p35uiwy4zk.js"
+    },
+    {
+      "hash": "sha256-dqpunZywlYy86b4O6ZjONasmG+4FH0P17FZ0OIf\/lIg=",
+      "url": "_framework\/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-HZOoaGpz9ekMa\/oSmeApSKp3DXNdVUq2UBqsTIIiN8A=",
+      "url": "_framework\/dotnet.runtime.8.0.1.fzde1kh9f7.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework\/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework\/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37\/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework\/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-hlrSdM7FzE8tGcgRCIaWCyMiK1Nh2JE4v+22i28sdm0=",
+      "url": "_framework\/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-fQhbSJBUlhsZdY27mqAFYWYQX8x2U0mjcNX8I+qxOMY=",
+      "url": "_framework\/BipSi.wasm"
+    },
+    {
+      "hash": "sha256-t4ppXQK0oVTK0NSnw6\/Bwq+FJj9OT6UK6a4DhxM7Qq4=",
+      "url": "_framework\/Microsoft.AspNetCore.Authorization.wasm"
+    },
+    {
+      "hash": "sha256-MdNVgc0cubEGKl8xL3P9ybipvFSaVaaYPYL5BRbl1no=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Authorization.wasm"
+    },
+    {
+      "hash": "sha256-MEonR+4tGXt+BzjWQaE1q24uQQb0kuVMFxiJ+TzzuFk=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-6Ii0xui1NKvy2fCRUKlkDuawuve\/InLIwkk05KvTY+0=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-Ws5szf6fWPAXna+XyWsAhavx+WDFcA\/91DQ7iPRPCMs=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-P9Hj\/2O8Roh1Q3Y1ORh4\/6kAfY\/fYWlp\/IfjEmGxHYI=",
+      "url": "_framework\/Microsoft.AspNetCore.Metadata.wasm"
+    },
+    {
+      "hash": "sha256-87sn2TYqgdZ95sXmavjKEzoEfMgHnYQ9LOvnMX+aZcI=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-Sxmy2ZS134URxbHEvdbS6NcQ3zXS7UWx\/5ZPpwiW7FA=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-jYqHUZ07UYWc8POk7xhas6xQYH7t1qoTcylqoDqncJk=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-0LFTLYGwRqd\/U8DSQaX+AL1PGuM5dcSLIubt0OxWZ+w=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-gg8xZqJsBBrrNEyUGzYqhL3sqkuZ4AHAvdTdL9nZ0S0=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-KiLtjw7P2FTzf6gXk1d+ET91506A6fqyNhUzxCW4PFk=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-8BH+kQfjYuZWxprOICXJ4+tU0OdJOYDKN7G0S3zYYHI=",
+      "url": "_framework\/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-6DN1malfyp0G0uOYwQY0nIN60XdXpVXUXXw0X2c8V8E=",
+      "url": "_framework\/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-6187ynEahDlSLMBvD4vAmiLpZ3clRb5xu6rM7O8AxNo=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-FJYig7i\/D2QMmzKroW6nNxHeb3zdOAAKOmp+AcH36ok=",
+      "url": "_framework\/Microsoft.IdentityModel.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-1\/YzjgaDsdJwYAH2ANglPS8A2M7pfNCJz4bPnsKDX\/s=",
+      "url": "_framework\/Microsoft.IdentityModel.JsonWebTokens.wasm"
+    },
+    {
+      "hash": "sha256-REBwuJ++YAGS747vvLVxNLEiKInbUcq0latDWHlcwQU=",
+      "url": "_framework\/Microsoft.IdentityModel.Logging.wasm"
+    },
+    {
+      "hash": "sha256-YaviQZjR85WzfO6zkAZVC5Fx0lKv51N25lDUABdfKUs=",
+      "url": "_framework\/Microsoft.IdentityModel.Tokens.wasm"
+    },
+    {
+      "hash": "sha256-lXwhdv0Is9FMH\/j0+WH3zoNWAGLbVaONtGucJ\/4URQ0=",
+      "url": "_framework\/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-CQVLCKiSxLN5ghyJZG3mhnMGRROdqrbNUjtAXB0nWb8=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-Wpl2E0FsKHzhZLWmO8fY0LM65P8qU9\/fKfAPF\/uIr80=",
+      "url": "_framework\/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-LureMJOfZttK4eGE0b7mTsESHsQncpvi9WZTVC3AcEQ=",
+      "url": "_framework\/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-\/6Kik6mC6ZHx8OXft116HbGv8VWDCF3oge\/a6pf8jB8=",
+      "url": "_framework\/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-tthBJwOnd2KLRrNxUYIGTPRqCwqAun\/U9DdiqUvoTHI=",
+      "url": "_framework\/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-tc5cNp2AdAb19E9oE97S3XSPP781074rqM4nseLApqo=",
+      "url": "_framework\/System.Diagnostics.DiagnosticSource.wasm"
+    },
+    {
+      "hash": "sha256-XOTGKjToqZ\/4\/2mXwGRHjA+6\/DCnrn2rZh+plL2Wtr0=",
+      "url": "_framework\/System.Formats.Asn1.wasm"
+    },
+    {
+      "hash": "sha256-imkKl05XqXdHBH\/La4w7StnpT1ybHeZTTpbkz5zkxIU=",
+      "url": "_framework\/System.IdentityModel.Tokens.Jwt.wasm"
+    },
+    {
+      "hash": "sha256-gM1VWXh7HXorwcAUWn8P+q04KD5NAAMnLrr\/WcoHLEI=",
+      "url": "_framework\/System.IO.Compression.wasm"
+    },
+    {
+      "hash": "sha256-x4eb4tMVctLs30Pk63DH+b8\/xxd09mKzVAQM3+0\/d98=",
+      "url": "_framework\/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-D3cYmwfcsfXcbJpvtPW9Bdahm+V0aYKnsLRqY90xr5Q=",
+      "url": "_framework\/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-EkPx0qkKM13cwOWiXzNVZ+hBtBdpmgAillgz2jLH++Y=",
+      "url": "_framework\/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-nF9fk722J9hXgRTF4PmISiSuCglcT8FA8U0NrFaluAg=",
+      "url": "_framework\/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-Wgf7MfLI7co+rZaUsGA1VZC1MpdUw0gd9XmP7\/CqtjU=",
+      "url": "_framework\/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-XwIcyPqyfGcpv5WMFfkv7SIzyxIv79jhJE5XFBohc+g=",
+      "url": "_framework\/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-fYGv7AZNQ2Bhmb\/rkZphhnaMPFe+Ex55hcATh2ImSPE=",
+      "url": "_framework\/System.Private.Xml.wasm"
+    },
+    {
+      "hash": "sha256-wIi66aTOmhZVzepyAzcEdbo2ANdpeESii99H6LjInq0=",
+      "url": "_framework\/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-onYe6nZMUQf+gz8vGu61yzicgII3Une\/ov4tXohEVjQ=",
+      "url": "_framework\/System.Runtime.InteropServices.wasm"
+    },
+    {
+      "hash": "sha256-\/hK54fBywVZF1VCuhtTV8gv3OjM94j6TsSjUzhZhSS4=",
+      "url": "_framework\/System.Runtime.Numerics.wasm"
+    },
+    {
+      "hash": "sha256-YlShc3MBLTaMJrSrGoBxKNAp2s6VJB4VCSS\/xPxEc2E=",
+      "url": "_framework\/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-Bk3T8AbO9qipmj3tZO9GY5i7ykokp6VRk8KbE3j\/5Pc=",
+      "url": "_framework\/System.Security.Claims.wasm"
+    },
+    {
+      "hash": "sha256-83YzrtAoJMFSrsFkd48J9npvZN7xWbL1uFEn2J6K9Bk=",
+      "url": "_framework\/System.Security.Cryptography.wasm"
+    },
+    {
+      "hash": "sha256-o03pYXjnCF1knoQKQXu6Lqw47uozvoQTpX715pSfRCM=",
+      "url": "_framework\/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-IT\/7jQj+wroPJ5tCEmDhq6TWXNxpE\/5sSuPH3LmF03M=",
+      "url": "_framework\/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-gBOmDgKZBuDoGsHbhJq\/ngBU9uslj8K\/+ZuZekYrq54=",
+      "url": "_framework\/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-Pxgvg2nf3wia2zLxbGiJ0DMnj3e9tiil0hGFFtvowhE=",
+      "url": "_framework\/System.Threading.Thread.wasm"
+    },
+    {
+      "hash": "sha256-Tb5aNrFcxn+mx3bkpjpEqdXhNjoPROpV0Q4Z+y4+a5I=",
+      "url": "_framework\/System.Threading.wasm"
+    },
+    {
+      "hash": "sha256-3cTubMEbm6U2X\/Oku4s1Od3gBUZyM+d1trTy3LVGaoM=",
+      "url": "_framework\/System.Xml.ReaderWriter.wasm"
+    },
+    {
+      "hash": "sha256-szYZJI0jDnC2SUYHJHWR1zxzQ1bKw3G12Sq28LOGpCw=",
+      "url": "_content\/Microsoft.AspNetCore.Components.WebAssembly.Authentication\/AuthenticationService.js"
+    }
+  ],
+  "version": "qREJRcD6"
+};
