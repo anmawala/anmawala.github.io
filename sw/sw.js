2d9cb6802fdfb81ad8049effd72f9284ca86f05d
Antonio Laviola <antonio.laviola@gmail.com>
2024-12-11 18:23:54 +0100

    Add initial service worker setup with HTML and manifest files

---
+++ sw/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '/sw/index.html',
+    '/sw/icons/icon-192x192.png',
+    '/sw/icons/icon-512x512.png',
+];
+
+// Install event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+            .catch(error => {
+                console.error('Failed to cache:', error);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Versione 2
