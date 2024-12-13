a7b1ad414fc47f63ff9f03af260e4670821f1b00
anmawala <antonio.laviola@gmail.com>
2024-12-13 11:55:39 +0100

    Update service worker registration to specify scope for improved functionality

--- pushapi/main.js
+++ pushapi/main.js
@@ -1,4 +1,4 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/pushapi/sw.js');
+    navigator.serviceWorker.register('/pushapi/sw.js', { scope: '/pushapi/' });
 }
