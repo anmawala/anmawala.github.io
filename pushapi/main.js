b378a6e202932a86da7277628d4e1bb2f50b5627
anmawala <antonio.laviola@gmail.com>
2024-12-13 11:57:22 +0100

    Add notification permission request after service worker registration

--- pushapi/main.js
+++ pushapi/main.js
@@ -2,3 +2,8 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/pushapi/sw.js', { scope: '/pushapi/' });
 }
+
+// Ask for permission to send notifications
+Notification.requestPermission(function (status) {
+    console.log('Notification permission status:', status);
+});
